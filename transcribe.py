fdb83d2db246d78e10e9390d5e0dd63f50c15445
maximofn <maximofn@gmail.com>
2023-10-25 20:06:14 +0200

    Manage files into transcribe.py

--- transcribe.py
+++ transcribe.py
@@ -1,9 +1,7 @@
 import os
 import argparse
 
-def main(args):
-    audio_file = args.input
-    language = args.language
+def transcribe(audio_file, language):
     output_folder = "transcriptions"
 
     # Transcribe audio file
@@ -21,7 +19,23 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Transcribe audio files')
-    parser.add_argument('input', help='Input audio file')
+    parser.add_argument('input_files', help='Input audio files')
     parser.add_argument('language', help='Language of the audio file')
+    parser.add_argument('speakers_file', help='File with the number of speakers')
     args = parser.parse_args()
-    main(args)
+
+    vocals_folder = "vocals"
+    extension = "wav"
+
+    with open(args.speakers_file, 'r') as f:
+        speakers = f.read().splitlines()
+        speakers = int(speakers[0])
+
+    with open(args.input_files, 'r') as f:
+        inputs = f.read().splitlines()
+    for input in inputs:
+        input, _ = input.split('.')
+        _, input_name = input.split('/')
+        for i in range(speakers):
+            file = f'{vocals_folder}/{input_name}_speaker{i:003d}.{extension}'
+            transcribe(file, args.language)
