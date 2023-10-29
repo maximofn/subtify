3dbe153abba912c56a9d478f4b0ae5203f611282
maximofn <maximofn@gmail.com>
2023-10-29 18:19:21 +0100

    Finish concatenate transcriptions script

--- concat_transcriptions.py
+++ concat_transcriptions.py
@@ -4,25 +4,44 @@ import re
 def sum_seconds(time, seconds):
     # Get time in seconds
     time = time.split(",")
-    milisecons = time[1]
+    time_milisecons = time[1]
+    time_milisecons = int(time_milisecons)/1000
     time = time[0].split(":")
     time = int(time[0])*3600 + int(time[1])*60 + int(time[2])
 
+    # Get integer and decimal part of seconds
+    seconds, seconds_miliseconds = divmod(seconds, 1)
+    seconds = int(seconds)
+    seconds_miliseconds = round(seconds_miliseconds, 3)
+
     # Add seconds
-    time += seconds
+    time = time + seconds
+    time_milisecons = time_milisecons + seconds_miliseconds
+    if time_milisecons >= 1:
+        time = time + 1
+        time_milisecons = time_milisecons - 1
+        time_milisecons = round(time_milisecons, 3)
 
     # Get time in hh:mm:ss,mmm format
-    hours = time // 3600
-    minutes = (time % 3600) // 60
-    seconds = (time % 3600) % 60
-    time = f"{hours:02d}:{minutes:02d}:{seconds:02d},{milisecons}"
+    hours = int(time) // 3600
+    minutes = (int(time) % 3600) // 60
+    seconds = (int(time) % 3600) % 60
+    time_milisecons = str(time_milisecons).split(".")[1]
+    time = f"{hours:02d}:{minutes:02d}:{seconds:02d},{time_milisecons}"
 
     return time
 
-def main(args):
-    chunk_files = args.chunk_files
-    seconds = int(args.seconds)
-    speaker = int(args.speaker)
+def hmsms_to_seconds(time):
+    # Get time in seconds
+    time = time.split(",")
+    milisecons = time[1]
+    time = time[0].split(":")
+    time = int(time[0])*3600 + int(time[1])*60 + int(time[2])
+    time = time + int(milisecons)/1000
+
+    return time
+
+def concatenate_transcriptions(chunk_files, seconds):
     chunk_folder = "transcriptions"
     output_folder = "concatenated_transcriptions"
     transcription_extension = "srt"
@@ -38,7 +57,7 @@ def main(args):
         chunk = file
         _, file = chunk.split("/")
         file, _ = file.split(".")
-        transcription_chunk_file = f"{chunk_folder}/{file}_speaker{speaker:003d}.{transcription_extension}"
+        transcription_chunk_file = f"{chunk_folder}/{file}.{transcription_extension}"
         with open(transcription_chunk_file, "r") as f:
             transcription_chunk = f.read().splitlines()
         for line in transcription_chunk:
@@ -49,7 +68,7 @@ def main(args):
                 start, end = line.split(" --> ")
                 # Add seconds to start and end time
                 start = sum_seconds(start, i*seconds)
-                end = sum_seconds(end, seconds)
+                end = sum_seconds(end, i*seconds)
                 # Add to transcription
                 transcription += f"{start} --> {end}\n"
             
@@ -64,7 +83,7 @@ def main(args):
     # Write transcription
     file_split = file.split("_")[:-1]
     file = "_".join(file_split)
-    output_file = f"{output_folder}/{file}_speaker{speaker:003d}.{transcription_extension}"
+    output_file = f"{output_folder}/{file}.{transcription_extension}"
     with open(output_file, "w") as f:
         f.write(transcription)
 
@@ -72,6 +91,17 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("chunk_files", help="Path to the file containing the paths to the chunk files")
     parser.add_argument("seconds", help="Duration of each chunk in seconds")
-    parser.add_argument("speaker", help="Speaker name")
+    parser.add_argument('speakers_file', help='File with the number of speakers')
     args = parser.parse_args()
-    main(args)
+
+    chunk_files = args.chunk_files
+    seconds = int(args.seconds)
+    with open(args.speakers_file, 'r') as f:
+        speakers = f.read().splitlines()
+        speakers = int(speakers[0])
+
+    if speakers > 0:
+        for speaker in range(speakers):
+            pass
+    else:
+        concatenate_transcriptions(chunk_files, seconds)
